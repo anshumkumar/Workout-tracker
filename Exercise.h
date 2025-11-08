ec5bfa610dee3837ee148f5428b681305ff0532d
anshumkumar <anshum1010@hotmail.com>
2025-11-08 19:08:21 +0000

    (Version 1.4) Started adding variables for weightlifting

--- Exercise.h
+++ Exercise.h
@@ -9,12 +9,16 @@ using namespace std;
 
 class Exercise {  
 protected:  
-string workout_name;   // Variable to store workout names  
-int workout_time;      // Variable to store workout time.  
+string workout_name;   // Variable to store workout names (Cardio + WL)
+int workout_time;      // Variable to store workout time. (Cardio + WL)
+// WL = Weight Lifting
+
 
 public:  
 Exercise();  //Default constructor  
-Exercise(string w_n, int w_t);  //Constructor with parameters  
+Exercise(string w_n, int w_t);  //Constructor with parameters 
+
+
 
 virtual void displayInfo() const; // Virtual function to display workouts. 
 // Definition of the function is in cpp file.
--- Weight-lifting.h
+++ Weight-lifting.h
@@ -5,6 +5,14 @@
 #include <string>
 
 class Weightlifting : public Exercise {
+private:
+	int weight;  // Variable to store weight lifted in (kg)
+	int sets;    // Variable to store number of sets
+	int reps;    // Variable to store number of repetitions per set
+// These variables are kept private because they are relevant to weight lifting only.
+
+public:
+	
 
 
 };
